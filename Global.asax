8b6fd2df1ef688a13bf9181598f974a797436508
Wisii-N <u19096527@tuks.co.za>
2022-08-21 10:13:25 +0200

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HW04_u19096527.MvcApplication" Language="C#" %>
---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW04_u19096527.Models
+{
+    public class User
+    {
+        //DATA MEMBERS
+        public int _ID;
+        public string _Name;
+        public string _Surname;
+        public string _CellNumber;
+        public string _Email;
+        public string Password;
+        public string Location;
+
+        //DEFAULT CONSTRUCTORS
+
+        //METHODS
+
+        //PROPERTIES
+
+    }
+}
